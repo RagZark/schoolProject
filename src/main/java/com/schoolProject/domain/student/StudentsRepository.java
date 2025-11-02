c0756425e64639e22b90141a86a22493c58eb24b
Danver Vernetti <danver.ver@hotmail.com>
2025-11-02 15:23:41 -0300

    setting values of StudentRepositoryJDBC

--- src/main/java/com/schoolProject/domain/student/StudentsRepository.java
+++ src/main/java/com/schoolProject/domain/student/StudentsRepository.java
@@ -1,4 +1,14 @@
 package com.schoolProject.domain.student;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface StudentsRepository {
+
+    void enrollStudent(Student student) throws SQLException;
+
+    Student searchByCpf(CPF cpf);
+
+    List<Student> listAllEnrolledStudents();
+
 }
--- src/main/java/com/schoolProject/infra/student/StudentRepositoryJDBC.java
+++ src/main/java/com/schoolProject/infra/student/StudentRepositoryJDBC.java
@@ -1,4 +1,35 @@
 package com.schoolProject.infra.student;
 
-public class StudentRepositoryJDBC {
+import com.schoolProject.domain.student.CPF;
+import com.schoolProject.domain.student.Student;
+import com.schoolProject.domain.student.StudentsRepository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class StudentRepositoryJDBC implements StudentsRepository {
+
+    private final Connection connection;
+
+    public StudentRepositoryJDBC(Connection connection){
+        this.connection = connection;
+    }
+
+    @Override
+    public void enrollStudent(Student student) throws SQLException {
+        String sql = "INSERT INTO STUDENT VALUES (NAME, CPF, EMAIL)";
+        PreparedStatement ps = connection.prepareStatement(sql);
+    }
+
+    @Override
+    public Student searchByCpf(CPF cpf) {
+        return null;
+    }
+
+    @Override
+    public List<Student> listAllEnrolledStudents() {
+        return List.of();
+    }
 }
